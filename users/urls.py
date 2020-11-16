c5ff2d906fcb12ce4d4ecc576d3886ec20b11a35
Matt <m23.cullen@gmail.com>
2020-11-16 17:52:02 -0500

    add templates and urls for user registration

--- users/urls.py
+++ users/urls.py
@@ -1,9 +1,10 @@
 from django.conf.urls import include, url
-from users.views import dashboard
+from users.views import dashboard, register
 
 urlpatterns = [
     url(r"^accounts/", include("django.contrib.auth.urls")),
     url(r"^dashboard/", dashboard, name="dashboard"),
+    url(r"^register/", register, name="register"),
 ]
 
 # <!-- URLS provided by django.contrib.auth.urls -->
